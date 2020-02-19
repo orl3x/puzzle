12edd90c71b80f5e56e97a20ae156bdf47206875
orlando <orlex5313@gmail.com>
2020-02-19 17:05:33 -0600

    Se agregó la evaluacion del rompecabezas con tecla

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
 var containerCell = document.getElementById("container-cell");
 var containerPiece = document.getElementById("container-piece");
 var selectedPiece = null;
+var dialogElement = document.getElementById("dialog");
 
+document.onkeypress = keypress;
+
+//CREAR CASILLAS EN EL CONTENEDOR DE CELDAS
 createBoard();
+
+//CREAR PIEZAS EN EL CONTENEDOR DE LAS PIEZASE
 createPieces();
 
 
-function createCell(width,height){
+function createCell(width,height, position){
 	
 
 	var cellElement = document.createElement("div");
 	cellElement.style.width = width;
 	cellElement.style.height = height;
 	cellElement.style.border = "1px solid black";
-	//cellElement.style.backgroundColor="yellow";
+	cellElement.style.backgroundColor="yellow";
+	cellElement.dataset.position = position;
 	cellElement.onclick=clickCell;		
+
 
 	return cellElement;
 }
@@ -35,6 +43,7 @@ function createPiece(width, height, piece){
 	pieceElement.height=height;
 	pieceElement.style.border="1px solid black";
 	pieceElement.src=piece.image;
+	pieceElement.dataset.position = piece.position;
 	pieceElement.onclick=clickPiece;
 	
 	cellElement.appendChild(pieceElement);
@@ -50,7 +59,7 @@ function createBoard(){
 	height /=4;
 
 	for(var i=0; i<16; i++){
-		let cellElement = createCell(width, height);
+		let cellElement = createCell(width, height, i);
 		addCell(cellElement);
 	}
 }
@@ -100,3 +109,47 @@ function clickCell(e){
 		console.log("Seleccione una pieza.");
 	}
 }
+
+	function keypress(ke){
+		if(ke.keyCode == 101 || ke.keyCode == 69){
+			let result = evaluateBoard();
+			showDialog(result);
+		}
+	}
+
+	function showDialog(result){
+		var imgElement = dialogElement.children[0];
+		var textContent = dialogElement.children[1];
+		if(result){
+			imgElement.src = "https://media1.tenor.com/images/fd78a4cbce84a5bfe6355a5b3d7989c0/tenor.gif?itemid=14829888";
+			textContent.innerText = "Ganastes";
+		}else{
+		imgElement.src = "https://media1.tenor.com/images/0dc8c94adf3ae7a24327f276891870c9/tenor.gif?itemid=5247844";
+		textContent.innerText = "Perdistes";
+		returnPieces();
+		}
+		dialogElement.style.display = "block";
+	}
+
+	function evaluateBoard(){
+		var cells = containerCell.children;
+		for(cell of cells){
+			let piece = cell.children[0];
+			if(piece.dataset.position != cell.dataset.position){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	function returnPieces(){
+		let cells=containerCell.children;
+		let cellPieces=container.children;
+
+		for(cell of cells){
+			let position = cell.dataset.position;
+			let piece = cell.children[0];
+			cellPieces[position].appendChild(piece);
+		}
+
+	}
